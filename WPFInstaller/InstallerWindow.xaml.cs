a233797ee2cad7648ab76e2fa29ee184cd2c281a
quebectech <amosnj355@gmail.com>
2026-04-09 16:01:29 +0300

    v2.0.4

--- WPFInstaller/InstallerWindow.xaml.cs
+++ WPFInstaller/InstallerWindow.xaml.cs
@@ -14,7 +14,7 @@ namespace TelnetCommanderProInstaller
             Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles),
             "Telnet Commander Pro");
         
-        private string _newVersion = "2.0.3";
+        private string _newVersion = "2.0.4";
         private bool isUpdate = false;
         private string? installedVersion = null;
         
